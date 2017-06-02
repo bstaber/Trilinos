cae2064e59e8053a6cf33c28e113d6ded9bd5782
bstaber <brian.staber@gmail.com>
2017-06-02 23:07:33 +0200

    nmc > 91 to 500

--- arteries/gmrf_neumann/main.cpp
+++ arteries/gmrf_neumann/main.cpp
@@ -64,14 +64,14 @@ int main(int argc, char *argv[]){
     my_interface->get_media(n_cells_p1_med,n_nodes_p1_med,path_p1);
     if (parameters_file_1.is_open() && parameters_file_2.is_open() && parameters_file_3.is_open() && parameters_file_4.is_open()){
         
-        for (unsigned nmc=0; nmc<100; ++nmc){
+        for (unsigned nmc=0; nmc<500; ++nmc){
             for (int i=0; i<n_nodes_p1_med; ++i){
                 parameters_file_1 >> my_interface->w1_gmrf(i);
                 parameters_file_2 >> my_interface->w2_gmrf(i);
                 parameters_file_3 >> my_interface->w3_gmrf(i);
                 parameters_file_4 >> my_interface->w4_gmrf(i);
             }
-            if (nmc>79){
+            if (nmc>91){
             Teuchos::RCP<Newton_Raphson> Newton = Teuchos::rcp(new Newton_Raphson(*my_interface,*paramList));
             Newton->setParameters(*paramList);
             Newton->Initialization();
