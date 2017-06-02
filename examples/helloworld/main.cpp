7ca8588cd938eb0c45559f02e90583c9b0c8dd85
bstaber <brian.staber@gmail.com>
2017-06-02 12:17:01 +0200

    Helloworld added.

---
+++ examples/helloworld/main.cpp
@@ -0,0 +1,33 @@
+#include "Epetra_ConfigDefs.h"
+#ifdef HAVE_MPI
+#include "mpi.h"
+#include "Epetra_MpiComm.h"
+#else
+#include "Epetra_SerialComm.h"
+#endif
+
+#include "Teuchos_CommandLineProcessor.hpp"
+#include "Teuchos_StandardCatchMacros.hpp"
+#include "Teuchos_ParameterList.hpp"
+#include "Teuchos_XMLParameterListCoreHelpers.hpp"
+#include "Arteries_ModelC_gmrf_neumann.hpp"
+#include "Newton_Raphsonpp.hpp"
+#include <boost/math/special_functions/gamma.hpp>
+
+int main(int argc, char *argv[]){
+    
+#ifdef HAVE_MPI
+    MPI_Init(&argc, &argv);
+    Epetra_MpiComm Comm(MPI_COMM_WORLD);
+#else
+    Epetra_SerialComm Comm;
+#endif
+    
+    std::cout << "Hello from " << Comm.MyPID() << " out of " << Comm.NumProc() << "\n";
+    
+#ifdef HAVE_MPI
+    MPI_Finalize();
+#endif
+return 0;
+    
+}
