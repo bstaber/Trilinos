ee11cd8b0a795b624ad36960540546a5f40144e7
bstaber <brian.staber@gmail.com>
2017-06-20 13:55:50 +0200

    little print

--- src/NRL_ModelF.hpp
+++ src/NRL_ModelF.hpp
@@ -139,6 +139,8 @@ public:
     
     void get_constitutive_tensors(Epetra_SerialDenseMatrix & deformation_gradient, Epetra_SerialDenseVector & piola_stress, Epetra_SerialDenseMatrix & tangent_piola){
         
+        std::cout << m1 << std::setw(20) << m2 << std::setw(20) << beta3 << std::setw(20) << beta4 << std::setw(20) << beta5 << "\n";
+        
         double det = deformation_gradient(0,0)*deformation_gradient(1,1)*deformation_gradient(2,2)-deformation_gradient(0,0)*deformation_gradient(1,2)*deformation_gradient(2,1)-deformation_gradient(0,1)*deformation_gradient(1,0)*deformation_gradient(2,2)+deformation_gradient(0,1)*deformation_gradient(1,2)*deformation_gradient(2,0)+deformation_gradient(0,2)*deformation_gradient(1,0)*deformation_gradient(2,1)-deformation_gradient(0,2)*deformation_gradient(1,1)*deformation_gradient(2,0);
         
         Epetra_SerialDenseMatrix C(3,3);
