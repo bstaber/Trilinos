c8d1adb4d9699966280d14b64d4f04696b610781
bstaber <brian.staber@gmail.com>
2017-06-20 14:41:29 +0200

    objfun normalized

--- nrl/deterministic_objectivefun/objectiveFunction.hpp
+++ nrl/deterministic_objectivefun/objectiveFunction.hpp
@@ -89,7 +89,6 @@ public:
         interface->set_parameters(m1,m2,beta3,beta4,beta5);
         interface->set_plyagl(plyagl);
         
-        double partialRef = 0.0;
         double partialVal = 0.0;
         newton->Initialization();
         for (unsigned int i=data_bc.size()-1; i<data_bc.size(); ++i){
@@ -111,16 +110,12 @@ public:
             }
             
             for (unsigned int j=0; j<exp_cells.size(); ++j){
-                partialVal += (exx_comp(j)-my_exx[i+j*nloads])*(exx_comp(j)-my_exx[i+j*nloads]) + (eyy_comp(j)-my_eyy[i+j*nloads])*(eyy_comp(j)-my_eyy[i+j*nloads]) + (exy_comp(j)-my_exy[i+j*nloads])*(exy_comp(j)-my_exy[i+j*nloads]);
-                partialRef += my_exx[i+j*nloads]*my_exx[i+j*nloads] + my_eyy[i+j*nloads]*my_eyy[i+j*nloads] + my_exy[i+j*nloads]*my_exy[i+j*nloads];
+                partialVal += ( (exx_comp(j)-my_exx[i+j*nloads])*(exx_comp(j)-my_exx[i+j*nloads]) + (eyy_comp(j)-my_eyy[i+j*nloads])*(eyy_comp(j)-my_eyy[i+j*nloads]) + (exy_comp(j)-my_exy[i+j*nloads])*(exy_comp(j)-my_exy[i+j*nloads]) )/( my_exx[i+j*nloads]*my_exx[i+j*nloads] + my_eyy[i+j*nloads]*my_eyy[i+j*nloads] + my_exy[i+j*nloads]*my_exy[i+j*nloads] );
             }
         }
         
         Real val = 0.0;
-        Real ref = 0.0;
-        comm->SumAll(&partialRef,&ref,1);
         comm->SumAll(&partialVal,&val,1);
-        //val = val/ref;
         
         delete [] MyVals;
         return val;
