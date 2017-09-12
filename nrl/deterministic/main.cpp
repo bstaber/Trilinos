e55f1abc03d804b2ed905349963d9f943caaca6a
Brian <brian.staber@gmail.com>
2017-09-12 10:02:00 -0400

    Test

--- nrl/deterministic/main.cpp
+++ nrl/deterministic/main.cpp
@@ -52,7 +52,7 @@ MPI_Init(&argc, &argv);
     parameters(7) = Teuchos::getParameter<double>(paramList->sublist("TIMooney"),"beta5");
     double plyagl = Teuchos::getParameter<double>(paramList->sublist("TIMooney"),"angle");
     
-    for (unsigned int i=0; i<5; i++){
+    for (unsigned int i=0; i<3; i++){
         //parameters(i) = 0.0;
         parameters(i) = parameters(i)*1.0e3;
     }
