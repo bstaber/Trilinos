dd8d33ce0ff9b8522a8b59e0d2dc50fa97def886
Brian <brian.staber@gmail.com>
2017-09-12 12:16:40 -0400

    Back to previous setting
    Meters and Pa

--- src/Compressible_Mooney_Transverse_Isotropic.hpp
+++ src/Compressible_Mooney_Transverse_Isotropic.hpp
@@ -66,7 +66,7 @@ public:
             if(coord==0.0){
                 n_bc_dof+=3;
             }
-            if(coord==25.0){ ///1000.0
+            if(coord==25.0/1000.0){
                 n_bc_dof+=3;
             }
         }
@@ -82,7 +82,7 @@ public:
                 dof_on_boundary[indbc+2] = 3*inode+2;
                 indbc+=3;
             }
-            if (coord==25.0){ ///1000.0
+            if (coord==25.0/1000.0){
                 dof_on_boundary[indbc+0] = 3*inode+0;
                 dof_on_boundary[indbc+1] = 3*inode+dof;
                 dof_on_boundary[indbc+2] = 3*inode+2;
@@ -102,7 +102,7 @@ public:
         for (unsigned int inode=0; inode<Mesh->n_local_nodes_without_ghosts; ++inode){
             node = Mesh->local_nodes[inode];
             coord = Mesh->nodes_coord[3*node+dof];
-            if (coord==25.0){ ///1000.0
+            if (coord==25.0/1000.0){
                 v[0][StandardMap->LID(3*node+dof)] = displacement;
             }
         }
@@ -119,7 +119,7 @@ public:
                 F[0][StandardMap->LID(3*node+1)] = 0.0;
                 F[0][StandardMap->LID(3*node+2)] = 0.0;
             }
-            if (coord==25.0){ ///1000.0
+            if (coord==25.0/1000.0){
                 F[0][StandardMap->LID(3*node+0)]   = 0.0;
                 F[0][StandardMap->LID(3*node+dof)] = displacement;
                 F[0][StandardMap->LID(3*node+2)]   = 0.0;
