6c6b3fb14ebe4b7d853e83bf9bd8616105ec6011
Brian <brian.staber@gmail.com>
2017-09-18 11:06:46 -0400

    Super error removed

--- src/Biaxial_Test.hpp
+++ src/Biaxial_Test.hpp
@@ -4,14 +4,13 @@
 #include "tensor_calculus.hpp"
 #include "hyperelasticity_setup_pp.hpp"
 
-class TIMooney : public hyperelasticity_setup
+class Biaxial_Test : public hyperelasticity_setup
 {
 public:
     
-    TIMooney(Epetra_Comm & comm, Teuchos::ParameterList & Parameters){
+    Biaxial_Test(Epetra_Comm & comm, Teuchos::ParameterList & Parameters){
         
         std::string mesh_file = Teuchos::getParameter<std::string>(Parameters.sublist("Mesh"), "mesh_file");
-        n_ply = Teuchos::getParameter<int>(Parameters.sublist("Mesh"), "n_ply");
         Mesh = new mesh(comm, mesh_file);
         Comm = Mesh->Comm;
         
@@ -21,34 +20,26 @@ public:
         create_FECrsGraph();
         
         setup_dirichlet_conditions();
-        for (unsigned int e=0; e<Mesh->n_cells/n_ply; ++e){
-            for (unsigned int j=0; j<n_ply/2; ++j){
-                phase.push_back(0);
-                phase.push_back(1);
-            }
-        }
     }
     
-    ~TIMooney(){
+    ~Biaxial_Test(){
     }
     
     void set_parameters(Epetra_SerialDenseVector & x){
-        mu1 = x(0);
-        mu2 = x(1);
-        mu3 = x(2);
-        mu4 = x(3);
-        mu5 = x(4);
-        beta3 = x(5);
-        beta4 = x(6);
-        beta5 = x(7);
-        mu = 2.0*mu1 + 4.0*mu2 + 2.0*mu3;
+        alpha1 = x(0);
+        alpha2 = x(1);
+        alpha3 = x(2);
+        alpha4 = x(3);
+        alpha5 = x(4);
+        delta1 = x(5);
+        eta1   = x(6);
+        
         trm = mu4 + 2.0*mu5;
-        ptrmbeta4 = std::pow(trm,beta4);
-        ptrmbeta5 = std::pow(trm,beta5);
-    }
-    
-    void set_plyagl(double & Plyagl){
-        plyagl = Plyagl;
+        ptrmbeta4 = std::pow(trm,alpha4);
+        ptrmbeta5 = std::pow(trm,alpha5);
+        
+        beta1  = alpha4*ptrmbeta4/(alpha5*ptrmbeta5);
+        delta2 = 2.0*alpha1 + 4.0*alpha2 + 2.0*delta1 + 2.0*eta1*alpha4*ptrmbeta4;
     }
     
     void get_matrix_and_rhs(Epetra_Vector & x, Epetra_FECrsMatrix & K, Epetra_FEVector & F){
@@ -56,6 +47,7 @@ public:
     }
     
     void setup_dirichlet_conditions(){
+        
         n_bc_dof = 0;
         int dof = 1;
         double coord;
@@ -130,89 +122,28 @@ public:
     }
     
     void get_material_parameters(unsigned int & e_lid, unsigned int & gp){
-        int e_gid = Mesh->local_cells[e_lid];
-        if (phase[e_gid]==0){
-            cos_plyagl = std::cos(plyagl);
-            sin_plyagl = std::sin(plyagl);
-        }
-        else{
-            cos_plyagl = std::cos(plyagl);
-            sin_plyagl = -std::sin(plyagl);
-        }
     }
     
     void get_constitutive_tensors(Epetra_SerialDenseMatrix & deformation_gradient, Epetra_SerialDenseVector & piola_stress, Epetra_SerialDenseMatrix & tangent_piola){
         
         double det = deformation_gradient(0,0)*deformation_gradient(1,1)*deformation_gradient(2,2)-deformation_gradient(0,0)*deformation_gradient(1,2)*deformation_gradient(2,1)-deformation_gradient(0,1)*deformation_gradient(1,0)*deformation_gradient(2,2)+deformation_gradient(0,1)*deformation_gradient(1,2)*deformation_gradient(2,0)+deformation_gradient(0,2)*deformation_gradient(1,0)*deformation_gradient(2,1)-deformation_gradient(0,2)*deformation_gradient(1,1)*deformation_gradient(2,0);
         
-        Epetra_SerialDenseMatrix C(3,3);
-        Epetra_SerialDenseMatrix CC(3,3);
-        Epetra_SerialDenseMatrix ddJ5(6,6);
-        Epetra_SerialDenseVector LML(6);
-        Epetra_SerialDenseVector eye(6);
-        Epetra_SerialDenseVector dJ5(6);
-        Epetra_SerialDenseVector M(6);
-        Epetra_SerialDenseVector L(6);
-        Epetra_SerialDenseVector c(6);
-        
-        C.Multiply('T','N',1.0,deformation_gradient,deformation_gradient,0.0);
-        CC.Multiply('N','N',1.0,C,C,0.0);
-        
-        c(0) = C(0,0); c(1) = C(1,1); c(2) = C(2,2); c(3) = C(1,2); c(4) = C(0,2); c(5) = C(0,1);
-        
-        L(0) = (1.0/(det*det))*(C(1,1)*C(2,2)-C(1,2)*C(2,1));
-        L(1) = (1.0/(det*det))*(C(0,0)*C(2,2)-C(0,2)*C(2,0));
-        L(2) = (1.0/(det*det))*(C(0,0)*C(1,1)-C(0,1)*C(1,0));
-        L(3) = (1.0/(det*det))*(C(0,2)*C(1,0)-C(0,0)*C(1,2));
-        L(4) = (1.0/(det*det))*(C(0,1)*C(1,2)-C(0,2)*C(1,1));
-        L(5) = (1.0/(det*det))*(C(0,2)*C(2,1)-C(0,1)*C(2,2));
-        
-        M(0) = mu4*sin_plyagl*sin_plyagl+mu5*cos_plyagl*cos_plyagl;
-        M(1) = mu4*cos_plyagl*cos_plyagl+mu5*sin_plyagl*sin_plyagl;
-        M(2) = mu5;
-        M(3) = 0.0;
-        M(4) = 0.0;
-        M(5) = (mu4-mu5)*cos_plyagl*sin_plyagl;
-        
-        LML(0) = M(0)*L(0)*L(0)+2.0*M(4)*L(0)*L(4)+2.0*M(5)*L(0)*L(5)+M(2)*L(4)*L(4)+2.0*M(3)*L(4)*L(5)+M(1)*L(5)*L(5);
-        LML(1) = M(1)*L(1)*L(1)+2.0*M(4)*L(1)*L(3)+2.0*M(5)*L(1)*L(5)+M(2)*L(3)*L(3)+2.0*M(4)*L(3)*L(5)+M(0)*L(5)*L(5);
-        LML(2) = M(2)*L(2)*L(2)+2.0*M(3)*L(2)*L(3)+2.0*M(4)*L(2)*L(4)+M(1)*L(3)*L(3)+2.0*M(5)*L(3)*L(4)+M(0)*L(4)*L(4);
-        LML(3) = L(2)*(L(1)*M(3)+L(3)*M(2)+L(5)*M(4))+L(3)*(L(1)*M(1)+L(3)*M(3)+L(5)*M(5))+L(4)*(L(5)*M(0)+L(1)*M(5)+L(3)*M(4));
-        LML(4) = L(2)*(L(0)*M(4)+L(4)*M(2)+L(5)*M(3))+L(3)*(L(0)*M(5)+L(5)*M(1)+L(4)*M(3))+L(4)*(L(0)*M(0)+L(4)*M(4)+L(5)*M(5));
-        LML(5) = L(1)*(L(0)*M(5)+L(5)*M(1)+L(4)*M(3))+L(3)*(L(0)*M(4)+L(4)*M(2)+L(5)*M(3))+L(5)*(L(0)*M(0)+L(4)*M(4)+L(5)*M(5));
-        
-        eye(0) = 1.0; eye(1) = 1.0; eye(2) = 1.0; eye(3) = 0.0; eye(4) = 0.0; eye(5) = 0.0;
-        
-        double I1 = C(0,0) + C(1,1) + C(2,2);
-        double II1 = C(0,0)*C(0,0) + C(1,1)*C(1,1) + C(2,2)*C(2,2) + 2.0*C(1,2)*C(1,2) + 2.0*C(0,2)*C(0,2) + 2.0*C(0,1)*C(0,1);
-        double I2 = (1.0/2.0)*(I1*I1-II1);
-        double I3 = det*det;
-        double I4 = C(0,0)*M(0) + C(1,1)*M(1) + C(2,2)*M(2) + 2.0*C(0,1)*M(5) + 2.0*C(0,2)*M(4) + 2.0*C(1,2)*M(3);
-        double I5 = CC(0,0)*M(0) + CC(1,1)*M(1) + CC(2,2)*M(2) + 2.0*CC(0,1)*M(5) + 2.0*CC(0,2)*M(4) + 2.0*CC(1,2)*M(3);
-        double J5 = I5 - I1*I4 + I2*trm;
-        double pI3 = std::pow(I3,-beta3);
-        double pI4 = std::pow(I4,beta4);
-        double pJ5 = std::pow(J5,beta5);
-        
-        for (unsigned int i=0; i<6; ++i){
-            dJ5(i) = J5*L(i) - I3*LML(i);
-            piola_stress(i) = 2.0*mu1*eye(i) + 2.0*mu2*(I1*eye(i)-c(i)) + (2.0*mu3*det*det-mu)*L(i) + (2.0/ptrmbeta4)*pI4*M(i) + (2.0/ptrmbeta5)*pJ5*dJ5(i) - 2.0*trm*pI3*L(i);
-        }
-        
-        double scalarAB = 4.0*mu2;
+        double scalarAB = 4.0*alpha2;
         tensor_product(scalarAB,eye,eye,tangent_piola,0.0);
-        
-        scalarAB = -4.0*mu2;
+        scalarAB = -4.0*alpha2;
         sym_tensor_product(scalarAB,eye,eye,tangent_piola,1.0);
-        
-        scalarAB = 4.0*mu3*det*det;
+        scalarAB = 4.0*delta1*det*det;
         tensor_product(scalarAB,L,L,tangent_piola,1.0);
-        
-        scalarAB = -4.0*mu3*det*det+2.0*mu;
+        scalarAB = 2.0*delta2 - 4.0*delta1*det*det;
         sym_tensor_product(scalarAB,L,L,tangent_piola,1.0);
         
+        scalarAB = 4.0*eta1*alpha4*(alpha4-1.0)*pJ4/(J4*J4);
+        tensor_product(scalarAB,M,M,tangent_piola,1.0);
+        scalarAB = 4.0*eta1*beta1*alpha5*(alpha5-1.0)*pJ5/J5;
+        tensor_product(scalarAB,dJ5,dJ5,tangent_piola,1.0);
+        
         scalarAB = J5;
-        tensor_product(scalarAB,L,L,ddJ5,0.0);
+        tensor_product(scalarAB,L,L,ddJ5,1.0);
         scalarAB = -J5;
         sym_tensor_product(scalarAB,L,L,ddJ5,1.0);
         scalarAB = -I3;
@@ -222,19 +153,8 @@ public:
         sym_tensor_product(scalarAB,L,LML,ddJ5,1.0);
         sym_tensor_product(scalarAB,LML,L,ddJ5,1.0);
         
-        scalarAB = 4.0*beta4*pI4/(I4*ptrmbeta4);
-        tensor_product(scalarAB,M,M,tangent_piola,1.0);
         
-        scalarAB = 4.0*beta5*pJ5/(J5*ptrmbeta5);
-        tensor_product(scalarAB,dJ5,dJ5,tangent_piola,1.0);
         
-        scalarAB = 4.0*trm*beta3*pI3;
-        tensor_product(scalarAB,L,L,tangent_piola,1.0);
-        scalarAB = 4.0*trm*pI3;
-        sym_tensor_product(scalarAB,L,L,tangent_piola,1.0);
-        
-        ddJ5.Scale(4.0*pJ5/ptrmbeta5);
-        tangent_piola += ddJ5;
     }
     
     void get_constitutive_tensors_static_condensation(Epetra_SerialDenseMatrix & deformation_gradient, double & det, Epetra_SerialDenseVector & inverse_cauchy, Epetra_SerialDenseVector & piola_isc, Epetra_SerialDenseVector & piola_vol, Epetra_SerialDenseMatrix & tangent_piola_isc, Epetra_SerialDenseMatrix & tangent_piola_vol){
@@ -251,25 +171,18 @@ public:
     void get_stress_for_recover(Epetra_SerialDenseMatrix & deformation_gradient, double & det, Epetra_SerialDenseMatrix & piola_stress){
     }
     
-    double mu1;
-    double mu2;
-    double mu3;
-    double mu4;
-    double mu5;
-    double mu;
-    double trm;
-    double beta3;
-    double beta4;
-    double beta5;
+    double alpha1;
+    double alpha2;
+    double alpha3;
+    double alpha4;
+    double alpha5;
+    double delta1;
+    double delta2;
+    double eta1;
     
+    double trm;
     double ptrmbeta4;
     double ptrmbeta5;
-    
-    double plyagl;
-    double cos_plyagl;
-    double sin_plyagl;
-    int n_ply;
-    std::vector<int> phase;
     
 };
 
