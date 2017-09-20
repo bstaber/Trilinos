8655f9b7c98f3ac859349f72b838a24fb3a09b99
Brian <brian.staber@gmail.com>
2017-09-20 11:14:25 -0400

    New folder

---
+++ linearelaslam/main.cpp
@@ -0,0 +1,55 @@
+#include "Epetra_ConfigDefs.h"
+#ifdef HAVE_MPI
+#include "mpi.h"
+#include "Epetra_MpiComm.h"
+#else
+#include "Epetra_SerialComm.h"
+#endif
+
+#include "orthotropicRF_laminate.hpp"
+
+int main(int argc, char *argv[]){
+    
+    std::string    xmlInFileName = "";
+    
+    Teuchos::CommandLineProcessor  clp(false);
+    clp.setOption("xml-in-file",&xmlInFileName,"The XML file to read into a parameter list");
+    clp.setDocString("TO DO.");
+    
+    Teuchos::CommandLineProcessor::EParseCommandLineReturn
+    parse_return = clp.parse(argc,argv);
+    if( parse_return != Teuchos::CommandLineProcessor::PARSE_SUCCESSFUL ) {
+        std::cout << "\nEnd Result: TEST FAILED" << std::endl;
+        return parse_return;
+    }
+	
+#ifdef HAVE_MPI
+MPI_Init(&argc, &argv);
+    Epetra_MpiComm Comm(MPI_COMM_WORLD);
+#else
+    Epetra_SerialComm Comm;
+#endif
+    
+    Teuchos::RCP<Teuchos::ParameterList> paramList = Teuchos::rcp(new Teuchos::ParameterList);
+    if(xmlInFileName.length()) {
+        Teuchos::updateParametersFromXmlFile(xmlInFileName, inoutArg(*paramList));
+    }
+    
+    if (Comm.MyPID()==0){
+        paramList->print(std::cout,2,true,true);
+    }
+    
+    Teuchos::RCP<orthotropicRF_laminate> interface = Teuchos::rcp(new orthotropicRF_laminate(Comm,*paramList));
+    
+    //->setup_bcs(choose displacement)
+    //get_lhs_and_rhs
+    //apply_bcs
+    //solve
+    
+    
+#ifdef HAVE_MPI
+    MPI_Finalize();
+#endif
+return 0;
+    
+}
