82308ee03d5aad4b2c3eeb931d2d2a35cbcd9b18
Brian <brian.staber@gmail.com>
2017-09-21 10:11:17 -0400

    Better printing

--- examples/shinozuka/main.cpp
+++ examples/shinozuka/main.cpp
@@ -67,19 +67,22 @@ int main(int argc, char *argv[]){
     double scdOrderMoment = 0.0;
     double GRFNorm2 = 0.0;
     
+    if (Comm.MyPID()==0){
+        std::cout << "\n E(||V||^2)/npoints = ";
+    }
     for (unsigned int j=1; j<=nmc; ++j){
         RandomField->rng.seed(j);
         RandomField->generator(V,Mesh);
         V.Norm2(&GRFNorm2);
         scdOrderMoment += (double(j-1.0)/double(j))*scdOrderMoment + 1.0/double(j)*GRFNorm2;
         if (Comm.MyPID()==0){
-            std::cout << "\n E(||V||^2)/npoints = " << scdOrderMoment/V.GlobalLength() << "\n";
+            std::cout << scdOrderMoment/V.GlobalLength() << "\n";
         }
     }
     
     scdOrderMoment = scdOrderMoment/V.GlobalLength();
     if (Comm.MyPID()==0){
-        std::cout << "\n E(||V||^2)/npoints = " << scdOrderMoment << "\n";
+        std::cout << "\n\n E(||V||^2)/npoints = " << scdOrderMoment << "\n";
     }
     //double alpha = 1.0/(0.10*0.10); double beta = 10.0*0.10*0.10;
     //RandomField->icdf_gamma(V,G,alpha,beta);
