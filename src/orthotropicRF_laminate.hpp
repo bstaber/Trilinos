90e82ebd306ff355e79edd2e2f57c2ddc619b2b3
Brian <brian.staber@gmail.com>
2017-09-25 11:01:28 -0400

    Debugging

--- src/orthotropicRF_laminate.hpp
+++ src/orthotropicRF_laminate.hpp
@@ -209,15 +209,13 @@ public:
         
         double epsilon = 1.0e-6;
         Epetra_SerialDenseMatrix M(6,6);
-        double M1 = m1(e_lid*n_gauss_cells+gp) + epsilon;
-        double M2 = m2(e_lid*n_gauss_cells+gp) + epsilon;
-        double M3 = m3(e_lid*n_gauss_cells+gp);
-        double M4 = m4(e_lid*n_gauss_cells+gp) + epsilon;
-        double M5 = m5(e_lid*n_gauss_cells+gp) + epsilon;
+        double M1 = 1.0; //m1(e_lid*n_gauss_cells+gp) + epsilon;
+        double M2 = 1.0; //'m2(e_lid*n_gauss_cells+gp) + epsilon;
+        double M3 = 0.0; //m3(e_lid*n_gauss_cells+gp);
+        double M4 = 1.0; //m4(e_lid*n_gauss_cells+gp) + epsilon;
+        double M5 = 1.0; //m5(e_lid*n_gauss_cells+gp) + epsilon;
         
         transverse_isotropic_matrix(M,M1,M2,M3,M4,M5);
-        
-        std::cout << M;
         
         double c1 = 144.8969*1.0e9;
         double c2 = 14.2500*1.0e9;
