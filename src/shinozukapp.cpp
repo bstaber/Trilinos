1879f249041c2a180abcf6ae7e2ea099dcbec1f6
Brian <brian.staber@gmail.com>
2017-09-26 10:56:18 -0400

    New shinozuka function

--- src/shinozukapp.cpp
+++ src/shinozukapp.cpp
@@ -120,6 +120,69 @@ void shinozuka::generator_gauss_points(Epetra_SerialDenseVector & v, mesh & Mesh
     
 }
 
+void generator_one_gauss_point(Epetra_SerialDenseVector & v, mesh * Mesh, double & xi, double & eta, double & zeta){
+    
+    int node, e_gid;
+    int n_local_cells = Mesh.n_local_cells;
+    int n_gauss_cells = Mesh.n_gauss_cells;
+    double x, y, z;
+    double xi, eta, zeta;
+    double ti, tj, tk;
+    double si, sj, sk;
+    double psi, phi, w, arg;
+    
+    Epetra_SerialDenseVector vector_x(3);
+    Epetra_SerialDenseVector shape_functions(Mesh.el_type);
+    Epetra_SerialDenseMatrix matrix_X(3,Mesh.el_type);
+    
+    switch (Mesh.el_type){
+        case 4:
+            tetra4::shape_functions(shape_functions, xi, eta, zeta);
+            break;
+        case 8:
+            hexa8::shape_functions(shape_functions, xi, eta, zeta);
+            break;
+        case 10:
+            tetra10::shape_functions(shape_functions, xi, eta, zeta);
+            break;
+    }
+    
+    for (int i=1; i<=order; ++i){
+        ti = tau_beta<int>(i);
+        si = s_tau(ti);
+        
+        for (int j=1; j<=order; ++j){
+            tj = tau_beta<int>(j);
+            sj = s_tau(tj);
+            
+            for (int k=1; k<=order; ++k){
+                tk = tau_beta<int>(k);
+                sk = s_tau(tk);
+                psi = psi_(rng);
+                phi = phi_(rng);
+                w = std::sqrt(-std::log(psi));
+                
+                for (int e_lid=0; e_lid<n_local_cells; ++e_lid){
+                    e_gid = Mesh.local_cells[e_lid];
+                    for (unsigned inode=0; inode<Mesh.el_type; ++inode){
+                        node = Mesh.cells_nodes[Mesh.el_type*e_gid+inode];
+                        matrix_X(0,inode) = Mesh.nodes_coord[3*node+0];
+                        matrix_X(1,inode) = Mesh.nodes_coord[3*node+1];
+                        matrix_X(2,inode) = Mesh.nodes_coord[3*node+2];
+                    }
+                        vector_x.Multiply('N','N',1.0,matrix_X,shape_functions,0.0);
+                        arg = 2.0*M_PI*phi + (M_PI/l1)*ti*vector_x(0) + (M_PI/l2)*tj*vector_x(1) + (M_PI/l3)*tk*vector_x(2);
+                        v(e_lid+gp) += std::sqrt(2.0*si*sj*sk)*w*std::cos(arg);
+                }
+                
+            }
+        }
+    }
+    
+}
+    
+}
+
 void shinozuka::icdf_gamma(Epetra_Vector & V, Epetra_Vector & G, double & alpha, double & beta){
     for (unsigned int i=0; i<V.MyLength(); ++i){
         double erfx = boost::math::erf<double>(V[i]);
