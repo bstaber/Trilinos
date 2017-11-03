4893f9a90c54d5a6c93a04e36d53570bedf42a90
Brian <brian.staber@gmail.com>
2017-11-02 20:48:46 -0400

    Error in k5

--- src/Compressible_Mooney_Transverse_Isotropic_Random_Field.hpp
+++ src/Compressible_Mooney_Transverse_Isotropic_Random_Field.hpp
@@ -95,19 +95,18 @@ public:
         double alpha, beta;
         for (unsigned int i=0; i<w1_shino.Length(); ++i){
             alpha = 1.0/(omega(0)*omega(0)); beta = mean_mu(0)*omega(0)*omega(0);
-            std::cout << alpha << std::setw(10) << beta << "\n";
             mu1rf(i) = icdf_gamma(w1_shino(i),alpha,beta);
+            
             alpha = 1.0/(omega(1)*omega(1)); beta = mean_mu(1)*omega(1)*omega(1);
-            std::cout << alpha << std::setw(10) << beta << "\n";
             mu2rf(i) = icdf_gamma(w2_shino(i),alpha,beta);
+            
             alpha = 1.0/(omega(2)*omega(2)); beta = mean_mu(2)*omega(2)*omega(2);
-            std::cout << alpha << std::setw(10) << beta << "\n";
             mu3rf(i) = icdf_gamma(w3_shino(i),alpha,beta);
+            
             alpha = 1.0/(omega(3)*omega(3)); beta = mean_mu(3)*omega(3)*omega(3);
-            std::cout << alpha << std::setw(10) << beta << "\n";
             mu4rf(i) = icdf_gamma(w4_shino(i),alpha,beta);
-            alpha = (2.0/alpha)-2.0; beta = mean_mu(4)/alpha;
-            std::cout << alpha << std::setw(10) << beta << "\n";
+            
+            alpha = (2.0*alpha)-1.0; beta = mean_mu(4)/alpha;
             mu5rf(i) = icdf_gamma(w5_shino(i),alpha,beta);
         }
         
