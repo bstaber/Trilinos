961b9a96e156df5961a4cc1ca10cea3ea72b06dd
Brian <brian.staber@gmail.com>
2017-11-08 14:08:13 -0500

    Printing newton

--- nrl/forward_randomfield/main.cpp
+++ nrl/forward_randomfield/main.cpp
@@ -74,7 +74,7 @@ MPI_Init(&argc, &argv);
                 for (int k=0; k<5; ++k){
                     seeds(k) = 5*j+k;
                 }
-                double value = costFunction->value(parameters,exponents,hyperParameters,id,seeds,false);
+                double value = costFunction->value(parameters,exponents,hyperParameters,id,seeds,true);
                 std::string path1 = "/home/s/staber/Trilinos_results/nrl/forward_randomfield/gpa_u_delta" + std::to_string(I) + "_L" + std::to_string(J) + "_nmc" + std::to_string(j) + ".mtx";
                 std::string path2 = "/home/s/staber/Trilinos_results/nrl/forward_randomfield/gpa_e_delta" + std::to_string(I) + "_L" + std::to_string(J) + "_nmc" + std::to_string(j) + ".mtx";
                 costFunction->print_newton_solution(path1);
