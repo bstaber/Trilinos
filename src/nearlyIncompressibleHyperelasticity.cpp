be7147d1780b5caae7d86256591d5146d50527c5
Brian <brian.staber@gmail.com>
2017-11-20 00:18:22 -0500

    Random space

--- src/nearlyIncompressibleHyperelasticity.cpp
+++ src/nearlyIncompressibleHyperelasticity.cpp
@@ -11,4 +11,3 @@ void nearlyIncompressibleHyperelasticity::assembleMixedDirichletDeformationDepen
 
 void nearlyIncompressibleHyperelasticity::stiffnessRhsMaterialContribution(Epetra_Vector & u, Epetra_FECrsMatrix & K, Epetra_FEVector & F);
 void nearlyIncompressibleHyperelasticity::stiffnessRhsPressureContribution(Epetra_Vector & u, Epetra_FECrsMatrix & K, Epetra_FEVector & F);
-
