1db8c126382d75b20a123a5aa2f55357afdbecbd
Brian <brian.staber@gmail.com>
2017-11-19 20:29:54 -0500

    Setting up files

--- src/compressibleHyperelasticity.cpp
+++ src/compressibleHyperelasticity.cpp
@@ -6,3 +6,18 @@ compressibleHyperelasticity::compressibleHyperelasticity(){
 compressibleHyperelasticity::~compressibleHyperelasticity(){
 }
 
+void compressibleHyperelasticity::assemblePureDirichlet_homogeneousForcing(Epetra_Vector & x, Epetra_FECrsMatrix & K, Epetra_FEVector & F){
+}
+void compressibleHyperelasticity::assemblePureDirichlet_inhomogeneousForcing(Epetra_Vector & x, Epetra_FECrsMatrix & K, Epetra_FEVector & F){
+}
+void compressibleHyperelasticity::assembleMixedDirichletNeumann_homogeneousForcing(Epetra_Vector & x, Epetra_FECrsMatrix & K, Epetra_FEVector & F){
+}
+void compressibleHyperelasticity::assembleMixedDirichletNeumann_inhomogeneousForcing(Epetra_Vector & x, Epetra_FECrsMatrix & K, Epetra_FEVector & F){
+}
+
+void compressibleHyperelasticity::stiffnessRhs_homogeneousForcing(Epetra_Vector & u, Epetra_FECrsMatrix & K, Epetra_FEVector & F){
+}
+void compressibleHyperelasticity::stiffnessRhs_inhomogeneousForcing(Epetra_Vector & u, Epetra_FECrsMatrix & K, Epetra_FEVector & F){
+}
+void compressibleHyperelasticity::rhs_NeumannBoundaryCondition(Epetra_FEVector & F){
+}
--- src/compressibleHyperelasticity.hpp
+++ src/compressibleHyperelasticity.hpp
@@ -8,5 +8,14 @@ class compressibleHyperelasticity : public hyperelasticity
 public:
     compressibleHyperelasticity();
     ~compressibleHyperelasticity();
+    
+    void assemblePureDirichlet_homogeneousForcing(Epetra_Vector & x, Epetra_FECrsMatrix & K, Epetra_FEVector & F);
+    void assemblePureDirichlet_inhomogeneousForcing(Epetra_Vector & x, Epetra_FECrsMatrix & K, Epetra_FEVector & F);
+    void assembleMixedDirichletNeumann_homogeneousForcing(Epetra_Vector & x, Epetra_FECrsMatrix & K, Epetra_FEVector & F);
+    void assembleMixedDirichletNeumann_inhomogeneousForcing(Epetra_Vector & x, Epetra_FECrsMatrix & K, Epetra_FEVector & F);
+    
+    void stiffnessRhs_homogeneousForcing(Epetra_Vector & u, Epetra_FECrsMatrix & K, Epetra_FEVector & F);
+    void stiffnessRhs_inhomogeneousForcing(Epetra_Vector & u, Epetra_FECrsMatrix & K, Epetra_FEVector & F);
+    void rhs_NeumannBoundaryCondition(Epetra_FEVector & F);
 };
 #endif
