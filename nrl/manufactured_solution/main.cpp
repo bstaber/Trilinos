462f83da868b397f13901130d5effda9364282a0
Brian <brian.staber@gmail.com>
2017-11-24 17:14:25 -0500

    OOps

--- nrl/manufactured_solution/main.cpp
+++ nrl/manufactured_solution/main.cpp
@@ -70,7 +70,7 @@ MPI_Init(&argc, &argv);
         //Newton->print_newton_solution(path1);
         errorL2(i) = manufactured->errorL2(*Newton->x);
     }
-    if (Comm->MyPID()==0){
+    if (Comm.MyPID()==0){
         std::cout << errorL2;
     }
     
