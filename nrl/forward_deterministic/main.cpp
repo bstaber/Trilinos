4c66f2c678a7657d078cc6dacaee561efcf7c169
Brian <brian.staber@gmail.com>
2017-12-19 11:31:44 -0500

    Improved readability

--- nrl/forward_deterministic/main.cpp
+++ nrl/forward_deterministic/main.cpp
@@ -62,49 +62,7 @@ MPI_Init(&argc, &argv);
             std::cout << "\n";
         }
     }
-    
-    /*Epetra_SerialDenseVector parameters(7);
-    Teuchos::RCP<TIMooney> interface = Teuchos::rcp(new TIMooney(Comm,*paramList));
-    
-    parameters(0)    = Teuchos::getParameter<double>(paramList->sublist("TIMooney"),"mu1");
-    parameters(1)    = Teuchos::getParameter<double>(paramList->sublist("TIMooney"),"mu2");
-    parameters(2)    = Teuchos::getParameter<double>(paramList->sublist("TIMooney"),"mu3");
-    parameters(3)    = Teuchos::getParameter<double>(paramList->sublist("TIMooney"),"mu4");
-    parameters(4)    = Teuchos::getParameter<double>(paramList->sublist("TIMooney"),"mu5");
-    parameters(5)    = Teuchos::getParameter<double>(paramList->sublist("TIMooney"),"beta4");
-    parameters(6)    = Teuchos::getParameter<double>(paramList->sublist("TIMooney"),"beta5");
-    for (unsigned int i=0; i<5; i++){
-        parameters(i) = 1.0e3*parameters(i);
-    }
-    
-    std::string pathnrl = Teuchos::getParameter<std::string>(paramList->sublist("nrldata"),"pathrnl");
-    Teuchos::RCP<readnrldata> nrldata = Teuchos::rcp(new readnrldata(true,pathnrl));
-    
-    double plyagldeg = nrldata->angles(0);
-    double plyagl    = 2.0*M_PI*plyagldeg/360.0;
-    
-    interface->set_parameters(parameters);
-    interface->set_plyagl(plyagl);
-    
-    Teuchos::RCP<Newton_Raphson> Newton = Teuchos::rcp(new Newton_Raphson(*interface,*paramList));
-    double xi = 0.0;
-    
-    Newton->Initialization();
-    for (unsigned int i=0; i<nrldata->boundaryconditions.Length(); ++i){
-        Newton->setParameters(*paramList);
-        if (i==0){
-            Newton->bc_disp = nrldata->boundaryconditions(i);
-        }
-        else{
-            Newton->bc_disp = nrldata->boundaryconditions(i)-nrldata->boundaryconditions(i-1);
-        }
-        int error = Newton->Solve_with_Aztec(true);
-        std::string path1 = "/home/s/staber/Trilinos_results/nrl/forward_deterministic/displacement_" + std::to_string(i) + ".mtx";
-        std::string path2 = "/home/s/staber/Trilinos_results/nrl/forward_deterministic/greenlag_" + std::to_string(i) + ".mtx";
-        Newton->print_newton_solution(path1);
-        interface->compute_green_lagrange(*Newton->x,xi,xi,xi,path2);
-    }*/
-    
+        
 #ifdef HAVE_MPI
     MPI_Finalize();
 #endif
