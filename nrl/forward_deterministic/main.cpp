6edd603e6eaf807dd6a6f12a7b45e2fe032986d5
Brian <brian.staber@gmail.com>
2018-01-15 21:28:43 -0500

    Testing RS solution 2.

--- nrl/forward_deterministic/main.cpp
+++ nrl/forward_deterministic/main.cpp
@@ -50,26 +50,65 @@ MPI_Init(&argc, &argv);
         x(i) = 1.0e3*x(i);
     }*/
     Epetra_SerialDenseVector x(7);
-    x(0) = 3.189704e+03;
-    x(1) = 8.441733e+00;
-    x(2) = 2.378051e+01;
-    x(3) = 3.325157e+02;
-    x(4) = 5.315200e+01;
-    x(5) = 2.951202e+00;
-    x(6) = 3.604109e-02;
+    x(0) = 2.739284e+03;
+    x(1) = 2.613957e+01;
+    x(2) = 4.524243e+01;
+    x(3) = 1.785786e+03;
+    x(4) = 3.367407e+01;
+    x(5) = 2.527997e+01;
+    x(6) = 4.677232e-02;
 
-{
-    Teuchos::RCP<forwardDeterministicCostFunction> obj =
-    Teuchos::rcp(new forwardDeterministicCostFunction(Comm,*paramList));
-    Epetra_SerialDenseVector value(10);
-    int id = 4;
-    value = obj->value_id(x,id);
-    if (Comm.MyPID()==0){
-      std::cout << "************ ID = " << id << " ************\n";
-      std::cout << value << "\n";
-      std::cout << "\n";
+    {
+        Teuchos::RCP<forwardDeterministicCostFunction> obj =
+        Teuchos::rcp(new forwardDeterministicCostFunction(Comm,*paramList));
+        Epetra_SerialDenseVector value(10);
+        int id = 0;
+        value = obj->value_id(x,id);
+        if (Comm.MyPID()==0){
+          std::cout << "************ ID = " << id << " ************\n";
+          std::cout << value << "\n";
+          std::cout << "\n";
+        }
     }
-}
+
+    {
+        Teuchos::RCP<forwardDeterministicCostFunction> obj =
+        Teuchos::rcp(new forwardDeterministicCostFunction(Comm,*paramList));
+        Epetra_SerialDenseVector value(10);
+        int id = 1;
+        value = obj->value_id(x,id);
+        if (Comm.MyPID()==0){
+          std::cout << "************ ID = " << id << " ************\n";
+          std::cout << value << "\n";
+          std::cout << "\n";
+        }
+    }
+
+    {
+        Teuchos::RCP<forwardDeterministicCostFunction> obj =
+        Teuchos::rcp(new forwardDeterministicCostFunction(Comm,*paramList));
+        Epetra_SerialDenseVector value(10);
+        int id = 4;
+        value = obj->value_id(x,id);
+        if (Comm.MyPID()==0){
+          std::cout << "************ ID = " << id << " ************\n";
+          std::cout << value << "\n";
+          std::cout << "\n";
+        }
+    }
+
+    {
+        Teuchos::RCP<forwardDeterministicCostFunction> obj =
+        Teuchos::rcp(new forwardDeterministicCostFunction(Comm,*paramList));
+        Epetra_SerialDenseVector value(10);
+        int id = 6;
+        value = obj->value_id(x,id);
+        if (Comm.MyPID()==0){
+          std::cout << "************ ID = " << id << " ************\n";
+          std::cout << value << "\n";
+          std::cout << "\n";
+        }
+    }
 
 #ifdef HAVE_MPI
     MPI_Finalize();
