e3bd6efa8afb1d655b908cff4707451b52849bc0
Brian <brian.staber@gmail.com>
2018-02-07 20:08:51 +0100

    Debugging

--- nrl/random_generator_for_pca_likelihood/RandomGeneratorForPCA_andLikelihood.hpp
+++ nrl/random_generator_for_pca_likelihood/RandomGeneratorForPCA_andLikelihood.hpp
@@ -58,13 +58,13 @@ public:
         omega(4) = correlation_lengths(0);
         omega(5) = correlation_lengths(1);
 
-        Epetra_SerialDenseVector GIndicator(nrldata->boundaryconditions.Length());
+        /*Epetra_SerialDenseVector GIndicator(nrldata->boundaryconditions.Length());
 
         interface->RandomFieldGenerator(seeds);
         interface->setParameters(mean_parameters,exponents,omega);
         interface->set_plyagl(plyagl);
 
-        /*newton->Initialization();
+        newton->Initialization();
         for (unsigned int i=0; i<nrldata->boundaryconditions.Length(); ++i){
             newton->setParameters(_paramList);
             if(i==0){
