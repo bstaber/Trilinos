b31ea5554db05188024d59c96bac177c1367d319
Brian <brian.staber@gmail.com>
2018-02-09 22:33:34 +0100

    Testing

--- nrl/test_parallel_shinozuka/main.cpp
+++ nrl/test_parallel_shinozuka/main.cpp
@@ -47,6 +47,42 @@ int main(int argc, char *argv[]){
     Teuchos::RCP<TIMooney_RandomField> Interface =
     Teuchos::rcp(new TIMooney_RandomField(Comm,*paramList));
 
+    Epetra_IntSerialDenseVector seeds(5);
+    Epetra_SerialDenseVector    mean_parameters(5);
+    Epetra_SerialDenseVector    exponents(2);
+    Epetra_SerialDenseVector    correlation_lengths(2);
+    Epetra_SerialDenseVector    coeff_of_variation(4);
+    Epetra_SerialDenseVector    plyagls(4);
+
+    //mean values of the random parameters G_1(x),...,G_5(x)
+    mean_parameters(0) = Teuchos::getParameter<double>(paramList->sublist("TIMooney"),"mu1");
+    mean_parameters(1) = Teuchos::getParameter<double>(paramList->sublist("TIMooney"),"mu2");
+    mean_parameters(2) = Teuchos::getParameter<double>(paramList->sublist("TIMooney"),"mu3");
+    mean_parameters(3) = Teuchos::getParameter<double>(paramList->sublist("TIMooney"),"mu4");
+    mean_parameters(4) = Teuchos::getParameter<double>(paramList->sublist("TIMooney"),"mu5");
+    mean_parameters.Scale(1.0e3);
+    //deterministic exponents beta_4 and beta_5
+    exponents(0) = Teuchos::getParameter<double>(paramList->sublist("TIMooney"),"beta4");
+    exponents(1) = Teuchos::getParameter<double>(paramList->sublist("TIMooney"),"beta5");
+    //correlation lengths of the Gaussian random field
+    omega(4) = Teuchos::getParameter<double>(paramList->sublist("Shinozuka"),"lx");
+    omega(5) = Teuchos::getParameter<double>(paramList->sublist("Shinozuka"),"ly");
+    //coefficients of variation of the random parameters G_1(x),...,G_4(x)
+    omega(0) = Teuchos::getParameter<double>(paramList->sublist("Shinozuka"),"delta1");
+    omega(1) = Teuchos::getParameter<double>(paramList->sublist("Shinozuka"),"delta2");
+    omega(2) = Teuchos::getParameter<double>(paramList->sublist("Shinozuka"),"delta3");
+    omega(3) = Teuchos::getParameter<double>(paramList->sublist("Shinozuka"),"delta4");
+    //ply angle
+    plyagls(0) = 15.0*2.0*M_PI/360.0;
+    plyagls(1) = 30.0*2.0*M_PI/360.0;
+    plyagls(2) = 60.0*2.0*M_PI/360.0;
+    plyagls(3) = 75.0*2.0*M_PI/360.0;
+
+    for (unsigned int i=0; i<5; ++i){ seeds(i) = i;};
+    interface->setParameters(mean_parameters,exponents,omega);
+    interface->set_plyagl(plyagl);
+    interface->RandomFieldGenerator(seeds);
+
 #ifdef HAVE_MPI
     MPI_Finalize();
 #endif
