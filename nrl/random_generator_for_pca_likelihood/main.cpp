c453a16b513637fc6f21fd79568cde4b05b0043e
Brian <brian.staber@gmail.com>
2018-03-15 16:34:45 +0100

    Print disps

--- nrl/random_generator_for_pca_likelihood/main.cpp
+++ nrl/random_generator_for_pca_likelihood/main.cpp
@@ -110,7 +110,7 @@ int main(int argc, char *argv[]){
                              coeff_of_variation ,
                              plyagls(i)         ,
                              false              ,
-                             false              ,
+                             true               ,
                              false             );
         }
       }
