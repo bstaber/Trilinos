beda15c37b9b767690f180322cc2933597bc23e4
Brian <brian.staber@gmail.com>
2018-04-10 23:01:03 +0200

    Nmc=100, tests 12 and 17

--- uqnonlinhom/run.m
+++ uqnonlinhom/run.m
@@ -2,21 +2,30 @@ clc
 clear all
 close all
 
-%% QUADRATURE
+addpath Laguerre/
+addpath Jacobi/
+addpath Laguerre/polynomials/
 
-gen_laguerre_rule(nq,k1-1,0,1,'genLagRuleOutput');
+%% QUADRATURE
+order = 10;
+
+nq = 10;
+k1 = 1/100; theta1 = 100;
+k2 = 1/100; theta2 = 100;
+y1 = 5; y2 = 5;
+
+GenLaguerreRule(nq,k1-1,0,1,'genLagRuleOutput');
 x1 = dlmread('genLagRuleOutput_x.txt');
 w1 = dlmread('genLagRuleOutput_w.txt');
+delete('genLagRuleOutput_x.txt', 'genLagRuleOutput_w.txt', 'genLagRuleOutput_r.txt');
 
-delete('genLagRuleOutput_x.txt', 'genLagRuleOutput_w.txt');
-
-gen_laguerre_rule(nq,k2-1,0,1,'genLagRuleOutput');
+GenLaguerreRule(nq,k2-1,0,1,'genLagRuleOutput');
 x2 = dlmread('genLagRuleOutput_x.txt');
 w2 = dlmread('genLagRuleOutput_w.txt');
+delete('genLagRuleOutput_x.txt', 'genLagRuleOutput_w.txt', 'genLagRuleOutput_r.txt');
 
-delete('genLagRuleOutput_x.txt', 'genLagRuleOutput_w.txt');
-
-[x3, w3] = jacobi_rule(nq, y1, y2, 0, 1, 'jacobiRule');
+[x3, w3] = JacobiRule(nq,y1,y2,0,1,'jacobiRule');
+delete('jacobiRule_x.txt', 'jacobiRule_w.txt', 'jacobiRule_r.txt');
 
 [N1,N2,N3] = meshgrid(x1,x2,x3);
 [W1,W2,W3] = meshgrid(w1,w2,w3);
@@ -24,31 +33,11 @@ delete('genLagRuleOutput_x.txt', 'genLagRuleOutput_w.txt');
 N1 = N1(:); N2 = N2(:); N3 = N3(:);
 W1 = W1(:); W2 = W2(:); W3 = W3(:);
 
-LAMBDA = theta1*N1; MU = theta2*N2;
+X1 = theta1*N1; 
+X2 = theta2*N2;
+X3 = 2*X2.*N3+X1;
 
-c1f = 100*c1;
-c2f = 100*c2; alphaf = c2f/2;
-lambdaf = c1f - (2/3)*c2f; 
-
-%% SOLVER
-
-F = [1.5, 0.3; 
-     0.3, 1.5]; 
- 
-H = F-eye(2);
-
-for nQ = 1:50
-    
-WEFF = convergenceAnalysis(nQ,H,FIXEDNODES,p,NE,GDOF,ND,...
-    PHASES,sb,sc,Ae,DOFe,ISPARSE,JSPARSE,KSPARSE);
-
-save(strcat(['WEFF_genLaguerre_n=', num2str(nQ), '.mat']), ...
-    'WEFF', 'N1', 'N2', 'LAMBDA', 'MU', 'k1', 'k2', 'theta1', 'theta2', ...
-    'c1', 'c2', 'c1f', 'c2f', 'lambda', 'lambdaf', 'delta1', 'delta2');
-
-end
-
-
-
-
+phi1 = repmat(sqrt(gamma(k1)*factorial(0:order)./gamma(k1+(0:order))),length(x1),1).*lf_function(length(x1),order,k1-1,x1);
+phi2 = repmat(sqrt(gamma(k1)*factorial(0:order)./gamma(k1+(0:order))),length(x2),1).*lf_function(length(x2),order,k2-1,x2);
+%phi3 = ...
 
