de3a1abf0f7735a9edd0304ceff41dfb07dadc50
Brian <brian.staber@gmail.com>
2018-04-20 09:36:28 +0200

    Testing other BCS

--- uqnonlinhom/StochasticHomogenization.hpp
+++ uqnonlinhom/StochasticHomogenization.hpp
@@ -85,7 +85,7 @@ public:
             y    = Mesh->nodes_coord[3*node+1];
             z    = Mesh->nodes_coord[3*node+2];
             if(x==0.0 || x==1.0){
-                v[0][StandardMap->LID(3*node+0)] = displacement*x;
+                v[0][StandardMap->LID(3*node+0)] = displacement;
                 v[0][StandardMap->LID(3*node+1)] = 0.0;
                 v[0][StandardMap->LID(3*node+2)] = 0.0;
             }
