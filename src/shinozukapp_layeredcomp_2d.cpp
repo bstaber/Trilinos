8ff8d97d3445ed6a99c28bd6f107d5c573ebae88
Brian <brian.staber@gmail.com>
2018-07-17 10:05:33 +0200

    Debugging

--- src/shinozukapp_layeredcomp_2d.cpp
+++ src/shinozukapp_layeredcomp_2d.cpp
@@ -61,6 +61,9 @@ void shinozuka_layeredcomp_2d::generator_gauss_points(Epetra_SerialDenseVector &
 
                     psi = psi_(rng);
                     phi = phi_(rng);
+
+                    std::cout << psi << std::setw(10) << phi << "\n";
+
                     w = std::sqrt(-std::log(psi));
                     k1    = (M_PI/l1)*ti*c + (M_PI/l2)*tj*s;
                     k2    = (M_PI/l2)*tj*c - (M_PI/l1)*ti*s;
