e1ca9446340662a2db6ccef98610f67e2bf0c5d5
Brian <brian.staber@gmail.com>
2018-09-22 23:58:24 +0200

    Removed useless class reference

--- examples/phasefield/phaseFieldProblem.hpp
+++ examples/phasefield/phaseFieldProblem.hpp
@@ -16,22 +16,22 @@ public:
   ~phaseFieldProblem(){
   }
 
-  Epetra_SerialDenseVector phaseFieldProblem::get_neumannBc(Epetra_SerialDenseMatrix & matrix_X, Epetra_SerialDenseMatrix & xg, unsigned int & gp){
+  Epetra_SerialDenseVector get_neumannBc(Epetra_SerialDenseMatrix & matrix_X, Epetra_SerialDenseMatrix & xg, unsigned int & gp){
       std::cout << "Not using this method in this application.\n";
       Epetra_SerialDenseVector f(3);
       return f;
   }
-  Epetra_SerialDenseVector phaseFieldProblem::get_forcing(double & x1, double & x2, double & x3, unsigned int & e_lid, unsigned int & gp){
+  Epetra_SerialDenseVector get_forcing(double & x1, double & x2, double & x3, unsigned int & e_lid, unsigned int & gp){
       std::cout << "Not using this method in this application.\n";
       Epetra_SerialDenseVector f(3);
       return f;
   }
 
-  void phaseFieldProblem::setup_dirichlet_conditions(){
+  void setup_dirichlet_conditions(){
 
   }
 
-  void phaseFieldProblem::apply_dirichlet_conditions(Epetra_FECrsMatrix & K, Epetra_FEVector & F, double & displacement){
+  void apply_dirichlet_conditions(Epetra_FECrsMatrix & K, Epetra_FEVector & F, double & displacement){
 
   }
 
